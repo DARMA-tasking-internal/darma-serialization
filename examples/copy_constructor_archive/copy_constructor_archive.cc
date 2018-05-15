f7ee865e50dc270381f25a428d65eabae2bd17df
David Hollman <dshollm@sandia.gov>
2018-05-14 19:55:56 -0700

    added copy-constructor-based archive as an example

---
+++ examples/copy_constructor_archive/copy_constructor_archive.cc
@@ -0,0 +1,173 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                      copy_constructor_archive.cc
+//                         DARMA
+//              Copyright (C) 2018 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact the DARMA developers (darma-admins@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#include <darma/serialization/serialization_traits.h>
+#include <darma/serialization/operator_overloads.h>
+
+#include <cstdlib>
+#include <type_traits>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <iostream>
+
+// An example of how to hook directly into copy constructor behavior using
+// the ADL-based customization points
+
+struct CopySizingArchive {
+  size_t size = 0;
+
+  void add_to_size_raw(size_t size) {
+    // should pretty much never be used, but whatever:
+    size += size;
+  }
+
+  static constexpr bool is_sizing() { return true; }
+  static constexpr bool is_packing() { return false; }
+  static constexpr bool is_unpacking() { return false; }
+  using is_sizing_archive_t = std::true_type;
+  using is_archive_t = std::true_type;
+};
+
+struct CopyPackingArchive {
+  char* spot = nullptr;
+
+  template <typename ContiguousIterator>
+  void pack_data_raw(ContiguousIterator begin, ContiguousIterator end) {
+    for(; begin != end; ++begin) {
+      *this | *begin;
+    }
+  }
+
+  static constexpr bool is_sizing() { return false; }
+  static constexpr bool is_packing() { return true; }
+  static constexpr bool is_unpacking() { return false; }
+  using is_packing_archive_t = std::true_type;
+  using is_archive_t = std::true_type;
+};
+
+template <typename Allocator=std::allocator<char>>
+struct CopyUnpackingArchive {
+  char* spot = nullptr;
+  using allocator_type = typename std::allocator_traits<Allocator>::template rebind_alloc<char>;
+  allocator_type allocator;
+
+  template <typename RawDataType>
+  void unpack_data_raw(void* allocated_dest, size_t n_items = 1) {
+    std::memcpy(allocated_dest, spot, n_items * sizeof(RawDataType));
+    spot += n_items * sizeof(RawDataType);
+  }
+
+  template <typename T>
+  T unpack_next_item_as() {
+    auto* old_spot = spot;
+    spot += sizeof(T);
+    return *(new T(std::move(*reinterpret_cast<T*>(old_spot))));
+  }
+
+  template <typename T>
+  void unpack_next_item_at(void* allocated) {
+    new (allocated) T(std::move(*reinterpret_cast<T*>(spot)));
+    spot += sizeof(T);
+  }
+
+  auto const& get_allocator() const {
+    return allocator;
+  }
+  auto& get_allocator() {
+    return allocator;
+  }
+
+  template <typename NeededAllocatorT>
+  NeededAllocatorT get_allocator_as() const {
+    // Let's hope this is a compatible type
+    return allocator;
+  }
+
+  static constexpr bool is_sizing() { return false; }
+  static constexpr bool is_packing() { return false; }
+  static constexpr bool is_unpacking() { return true; }
+  using is_unpacking_archive_t = std::true_type;
+  using is_archive_t = std::true_type;
+};
+
+
+// Ensure the "regular" serializer never gets used:
+template <typename T>
+void darma_compute_size(T const& obj, CopySizingArchive& ar) {
+  ar.size += sizeof(T);
+}
+
+// Ensure the "regular" serializer never gets used:
+template <typename T>
+void darma_pack(T const& obj, CopyPackingArchive& ar) {
+  new (ar.spot) T(obj);
+  ar.spot += sizeof(T);
+}
+
+// Ensure the "regular" serializer never gets used:
+template <typename T>
+void darma_unpack(darma_runtime::serialization::allocated_buffer_for<T> alloc, CopyUnpackingArchive<>& ar) {
+  ar.unpack_next_item_at<T>(alloc.pointer);
+}
+
+
+int main() {
+  int i = 42;
+  double val = 3.14;
+  std::string str = "hello world";
+
+  auto s_ar = CopySizingArchive{};
+  s_ar | i | val | str;
+
+  char buffer[s_ar.size];
+  auto p_ar = CopyPackingArchive{buffer};
+  p_ar | i | val | str;
+
+
+  i = 0; val = 0; str = "";
+  auto u_ar = CopyUnpackingArchive<>{buffer, std::allocator<char>()};
+  u_ar | i | val | str;
+  std::cout << i << ", " << val << ", " << str << std::endl;
+}
---
+++ source/include/darma/serialization/operator_overloads.h
@@ -0,0 +1,184 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                      operator_overloads.h
+//                         DARMA
+//              Copyright (C) 2018 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact the DARMA developers (darma-admins@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#ifndef DARMASERIALIZATION_OPERATOR_OVERLOADS_H
+#define DARMASERIALIZATION_OPERATOR_OVERLOADS_H
+
+#include "serialization_traits.h"
+#include "archive_concept.h"
+
+#include <type_traits>
+#include <memory>
+
+namespace darma_runtime {
+namespace serialization {
+namespace detail {
+
+template <typename Archive, typename T>
+using _has_darma_compute_size_archetype = decltype(
+  darma_compute_size(std::declval<T const&>(), std::declval<Archive&>())
+);
+
+template <typename Archive, typename T>
+using _has_darma_pack_archetype = decltype(
+  darma_pack(std::declval<T const&>(), std::declval<Archive&>())
+);
+
+template <typename Archive, typename T>
+using _has_darma_unpack_archetype = decltype(
+  darma_unpack(std::declval<darma_runtime::serialization::allocated_buffer_for<T>>(), std::declval<Archive&>())
+);
+
+} // end namespace detail
+} // end namespace serialization
+} // end namespace darma_runtime
+
+// Put heavily-constrained operator| overloads in the global namespace
+
+template <typename Archive, typename T>
+std::enable_if_t<
+  darma_runtime::serialization::is_archive_v<Archive>
+  and darma_runtime::serialization::is_sizing_archive_v<Archive>
+  and tinympl::is_detected_exact<void,
+    darma_runtime::serialization::detail::_has_darma_compute_size_archetype,
+    Archive, T
+  >::value,
+  Archive&
+>
+operator|(Archive& ar, T const& val) {
+  // Call with an unqualified name to trigger ADL
+  darma_compute_size(val, ar);
+  return ar;
+};
+
+template <typename Archive, typename T>
+std::enable_if_t<
+  darma_runtime::serialization::is_archive_v<Archive>
+    and darma_runtime::serialization::is_sizing_archive_v<Archive>
+    and tinympl::is_detected_exact<void,
+      darma_runtime::serialization::detail::_has_darma_compute_size_archetype,
+      Archive, T
+    >::value,
+  Archive&
+>
+operator%(Archive& ar, T const& val) {
+  // Call with an unqualified name to trigger ADL
+  darma_compute_size(val, ar);
+  return ar;
+};
+
+template <typename Archive, typename T>
+std::enable_if_t<
+  darma_runtime::serialization::is_archive_v<Archive>
+    and darma_runtime::serialization::is_packing_archive_v<Archive>
+    and tinympl::is_detected_exact<void,
+      darma_runtime::serialization::detail::_has_darma_pack_archetype,
+      Archive, T
+    >::value,
+  Archive&
+>
+operator|(Archive& ar, T const& val) {
+  // Call with an unqualified name to trigger ADL
+  darma_pack(val, ar);
+  return ar;
+};
+
+template <typename Archive, typename T>
+std::enable_if_t<
+  darma_runtime::serialization::is_archive_v<Archive>
+    and darma_runtime::serialization::is_packing_archive_v<Archive>
+    and tinympl::is_detected_exact<void,
+      darma_runtime::serialization::detail::_has_darma_pack_archetype,
+      Archive, T
+    >::value,
+  Archive&
+>
+operator<<(Archive& ar, T const& val) {
+  // Call with an unqualified name to trigger ADL
+  darma_pack(val, ar);
+  return ar;
+};
+
+
+template <typename Archive, typename T>
+std::enable_if_t<
+  darma_runtime::serialization::is_archive_v<Archive>
+    and darma_runtime::serialization::is_unpacking_archive_v<Archive>
+    and tinympl::is_detected_exact<void,
+      darma_runtime::serialization::detail::_has_darma_unpack_archetype,
+      Archive, T
+    >::value,
+  Archive&
+>
+operator|(Archive& ar, T& val) {
+  // First, destroy val
+  using alloc_t = typename std::allocator_traits<typename Archive::allocator_type>::template rebind_alloc<T>;
+  alloc_t alloc = ar.template get_allocator_as<alloc_t>();
+  std::allocator_traits<alloc_t>::destroy(alloc, &val);
+  // Call with an unqualified name to trigger ADL
+  darma_unpack(darma_runtime::serialization::allocated_buffer_for<T>{static_cast<void*>(&val)}, ar);
+  return ar;
+};
+
+template <typename Archive, typename T>
+std::enable_if_t<
+  darma_runtime::serialization::is_archive_v<Archive>
+    and darma_runtime::serialization::is_unpacking_archive_v<Archive>
+    and tinympl::is_detected_exact<void,
+      darma_runtime::serialization::detail::_has_darma_unpack_archetype,
+      Archive, T
+    >::value,
+  Archive&
+>
+operator>>(Archive& ar, T& val) {
+  // First, destroy val
+  using alloc_t = typename std::allocator_traits<typename Archive::allocator_type>::template rebind_alloc<T>;
+  alloc_t alloc = ar.template get_allocator_as<alloc_t>();
+  std::allocator_traits<alloc_t>::destroy(alloc, &val);
+  // Call with an unqualified name to trigger ADL
+  darma_unpack(darma_runtime::serialization::allocated_buffer_for<T>{static_cast<void*>(&val)}, ar);
+  return ar;
+};
+
+#endif //DARMASERIALIZATION_OPERATOR_OVERLOADS_H
